b3d693217f9156eabe1941d2ea0c4f5f0a188fba
Emanuel Palm <emanuelpalm@gmail.com>
2026-05-19 22:54:57 +0200

    Add simple CLI parser; add --help and --version flags to binary

---
+++ src/cli.rs
@@ -0,0 +1,106 @@
+use std::fmt;
+use std::option::Option;
+
+pub struct PatternSet<const F: usize> {
+    pub description: &'static str,
+    pub flags: [Flag; F],
+}
+
+pub struct Flag {
+    pub short: Option<char>,
+    pub long: &'static str,
+    pub description: &'static str,
+    pub kind: FlagKind,
+}
+
+pub enum FlagKind {
+    Toggle,
+    String,
+}
+
+pub enum FlagValue {
+    None,
+    Toggled,
+    String(Option<String>),
+}
+
+pub struct MatchSet<const F: usize> {
+    pub args: Vec<String>,
+    pub flags: [FlagValue; F],
+    pub remainder: Vec<String>,
+}
+
+pub fn parse<const F: usize>(
+    pattern: &PatternSet<F>,
+    mut args: impl Iterator<Item = String>,
+) -> MatchSet<F> {
+    let mut match_set = MatchSet::<F> {
+        args: Vec::new(),
+        flags: [const { FlagValue::None }; F],
+        remainder: Vec::new(),
+    };
+
+    'outer: while let Some(arg) = args.next() {
+        if !arg.starts_with("-") {
+            match_set.args.push(arg);
+            continue;
+        }
+
+        if arg == "--" {
+            args.for_each(|arg| match_set.args.push(arg));
+            break;
+        }
+
+        for i in 0..F {
+            let flag = &pattern.flags[i];
+            if matches(flag, &arg) {
+                match_set.flags[i] = match flag.kind {
+                    FlagKind::Toggle => FlagValue::Toggled,
+                    FlagKind::String => FlagValue::String(match args.next() {
+                        Some(arg) if !arg.starts_with("-") => Some(arg),
+                        _ => None,
+                    }),
+                };
+                continue 'outer;
+            }
+        }
+
+        match_set.remainder.push(arg);
+    }
+
+    return match_set;
+
+    fn matches(flag: &Flag, arg: &str) -> bool {
+        arg.starts_with("--") && flag.long == &arg[2..]
+            || arg.len() == 2 && flag.short == Some(arg.as_bytes()[0] as char)
+    }
+}
+
+pub fn print<const F: usize, W: fmt::Write>(cmd: &PatternSet<F>, mut w: W) -> fmt::Result {
+    writeln!(w, "{}", cmd.description)?;
+
+    if !cmd.flags.is_empty() {
+        writeln!(w)?;
+        writeln!(w, "Options:")?;
+    }
+    for i in 0..F {
+        let flag = &cmd.flags[i];
+        w.write_str("  ")?;
+        if let Some(short) = flag.short {
+            w.write_char('-')?;
+            w.write_char(short)?;
+            w.write_char(',')?;
+        } else {
+            w.write_str("   ")?;
+        }
+        let value_str = match flag.kind {
+            FlagKind::Toggle => " ",
+            FlagKind::String => " <string>",
+        };
+        let value_len = 13 - value_str.len();
+
+        writeln!(w, " --{:<value_len$}{} {}", flag.long, value_str, flag.description)?;
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,43 @@
+use std::io::Write;
+use std::{env, io};
+
 mod ast;
+mod cli;
 mod codegen;
 mod vm;
 
+const ROOT_PATTERN: cli::PatternSet<2> = cli::PatternSet {
+    description: "The skript language toolkit.\n\nUsage: skript [options] <file>",
+    flags: [
+        cli::Flag {
+            short: Some('h'),
+            long: "help",
+            description: "Prints this help message and exits.",
+            kind: cli::FlagKind::Toggle,
+        },
+        cli::Flag {
+            short: None,
+            long: "version",
+            description: "Prints the current application version and exits.",
+            kind: cli::FlagKind::Toggle,
+        },
+    ],
+};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli::parse(&ROOT_PATTERN, env::args());
+
+    // Help
+    if let cli::FlagValue::Toggled = matches.flags[0] {
+        let mut buffer = String::new();
+        cli::print(&ROOT_PATTERN, &mut buffer).unwrap();
+        io::stdout().write_all(buffer.as_bytes()).unwrap();
+        return;
+    }
+
+    // Version
+    if let cli::FlagValue::Toggled = matches.flags[1] {
+        println!("0.0.1");
+        return;
+    }
 }
